389a22cddb75410c2dca0e7d915c00eef36f3d03
Luuuuukas <75544324+Luuuuukas@users.noreply.github.com>
2023-01-30 02:54:51 +0200

    Add files via upload

--- dinoGameScript.js
+++ dinoGameScript.js
@@ -1,21 +1,18 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-function jump(){
-    if(character.classList != "animate"){
-        character.classList.add("animate");
-    }
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },500)
-}
-
-var checkDead = setInterval(function(){
+var { character, block } = newFunction();
+const newLocal = setInterval(function () {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockleft<20 && blockleft>0 && characterTop>=130){
+    if (blockleft < 20 && blockleft > 0 && characterTop >= 130) {
         block.style.animation = "none";
         block.style.display = "none";
         alert("u lose.");
     }
-},10);
+}, 10);
+var checkDead = newLocal;
+
+function newFunction() {
+    var character = document.getElementById("character");
+    var block = document.getElementById("block");
+    return { character, block };
+}
+
