efb71d1da8238897f5ed16ecebb4c087a488bbd2
Luuuuukas <75544324+Luuuuukas@users.noreply.github.com>
2023-01-30 03:19:53 +0200

    Add files via upload

--- dinoGameScript.js
+++ dinoGameScript.js
@@ -16,12 +16,3 @@ function newFunction() {
     var block = document.getElementById("block");
     return { character, block };
 }
-function jump() {
-    if (character.classList != "animate") {
-        character.classList.add("animate");
-    }
-    character.classList.add("animate");
-    setTimeout(function () {
-        character.classList.remove("animate");
-    }, 500);
-}
